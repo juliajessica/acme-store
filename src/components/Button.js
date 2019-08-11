0b0687da15db2b9e192fa153f68f0d4e0fc152d0
julia sheremet <juliajessicasher@gmail.com>
2019-08-10 17:39:04 -0700

    WIP: create reusable button component with different states

---
+++ src/components/Button.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { string, node, oneOfType } from 'prop-types';
+import { ShoppingCart, Minus, Add } from '../assets/svgs';
+
+import './button.css';
+
+const Button = ({ children, customClass, theme, type, ...rest }) => {
+  const renderSvg = () => {
+    let svg = null;
+    if (theme === 'textLink') {
+      svg = <ShoppingCart />;
+    }
+    if (theme === 'secondary-minus') {
+      svg = <Minus />;
+    }
+    if (theme === 'secondary-add') {
+      svg = <Add />;
+    }
+    return svg;
+  };
+
+  return (
+    /* eslint-disable react/button-has-type */
+    <button type={type} className={`button_container button_${theme} ${customClass}`} {...rest}>
+      {renderSvg()}
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: oneOfType([string, node]),
+  customClass: string,
+  theme: string,
+  type: string
+};
+
+Button.defaultProps = {
+  children: null,
+  customClass: null,
+  theme: 'primary',
+  type: 'button'
+};
+
+export default Button;
